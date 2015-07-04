56b2fad67ad5e7602231824e6a9ca1aa481b636e
Y-NAKA <com.y.naka@gmail.com>
2015-07-04 11:12:18 +0900

    updated apikey

--- js/jumbotron-common.js
+++ js/jumbotron-common.js
@@ -8,7 +8,7 @@
 var VIEWER_ID = 'viewer' + location.hash.replace(/^#/, '');
 
 // SkyWay (jt.skyway.io)
-var LICENSE_KEY = '5528079a-63d8-11e4-b3d7-530ce53754b0';
+var LICENSE_KEY = 'c2d8c99f-4902-445b-8ee1-76c8294249e1';
 
 // SkyWay (localhost)
 //var LICENSE_KEY = '6165842a-5c0d-11e3-b514-75d3313b9d05';
