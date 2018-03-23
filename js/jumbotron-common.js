779968b47deb0f66955dd142cfae228b8e67ea18
Ryosuke Otsuya <r.otsuya@gmail.com>
2018-03-23 11:26:33 +0900

    Comment about SkyWay's API key

--- js/jumbotron-common.js
+++ js/jumbotron-common.js
@@ -1,6 +1,6 @@
 var VIEWER_ID = 'viewer' + location.hash.replace(/^#/, '');
 
-// SkyWay (jt.skyway.io)
+// SkyWay (jt.skyway.io and localhost, P2P only, TURN and SFU is disabled)
 var LICENSE_KEY = 'bf09a433-41d9-4e2b-9229-cb711a8a7306';
 
 // URLs
